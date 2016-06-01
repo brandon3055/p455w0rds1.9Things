e3ed6ca6c8e8c2e243bca29c92705fdd6ca445cc
p455w0rd <p455w0rd@gmail.com>
2016-06-01 01:37:52 -0400

    new stuff/testing github IRC bot

--- src/java/p455w0rd/p455w0rdsthings/Globals.java
+++ src/java/p455w0rd/p455w0rdsthings/Globals.java
@@ -6,7 +6,7 @@ public class Globals {
 	public static final String NAME = "p455w0rd's Things";
 	public static final String SERVER_PROXY_CLASS = "p455w0rd.p455w0rdsthings.proxy.CommonProxy";
 	public static final String CLIENT_PROXY_CLASS = "p455w0rd.p455w0rdsthings.proxy.ClientProxy";
-	public static final String GUI_FACTORY = "p455w0rd.client.gui.GuiFactory";
+	public static final String GUI_FACTORY = "p455w0rd.p455w0rdsthings.client.gui.GuiFactory";
 	
 	private static int guiIndex = 0;
 	public static int GUINUM_DANKNULL = ++guiIndex;
